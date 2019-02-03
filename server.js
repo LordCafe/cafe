eb86b7b982c2cda00de1a7c4ad91e221f38dd6a4
Adrian Lopez <adrian@imagendigital.com>
2019-02-03 14:19:22 -0600

    stable 1

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const  path= require('path');
+var expressLayouts = require('express-ejs-layouts');
+const port = process.env.PORT || 3000;
+
+const PUBLIC = path.join(__dirname + '/public')
+
+// Set the default templating engine to ejs
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(  express.static(PUBLIC));
+
+
+
+app.get('/', (req, res) => {
+	res.render('index',{ title: 'Page title' });
+});
+
+app.listen( port, () => console.log(`welcome to ${port}`));
